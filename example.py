fa8000a23a20bf057adaf476742bdbec1fdf99a0
Gabriel <gabriel.zima@gmail.com>
2018-08-09 15:38:00 +0200

    Small change :)

--- example.py
+++ example.py
@@ -1,8 +1,9 @@
 import c14_class
 
 c14 = C14_RS485('/dev/ttyUSB3')
-arTemp = c14.ReadValues('T', 100, 21, [1, 2, 3])
+arValNum = [1, 2, 3]
+arTemp = c14.ReadValues('T', 100, 21, arValNum)
 if len(arTemp) == 30:
-  print("Temp 1 = " + str(arTemp[0]))
-  print("Temp 2 = " + str(arTemp[1]))
-  print("Temp 3 = " + str(arTemp[2]))
+    for i in arValNum:
+        print("Temperatura nr: " + str(arValNum[i]) + " = " + str(arTemp[i]) + "*C")
+
