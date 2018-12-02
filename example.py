fd1d6b15ba183c28536ad42485d028c54d29fb54
Gabriel <gabriel.zima@gmail.com>
2018-12-02 14:56:39 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,6 +1,5 @@
 import c14_class
 
 c14 = c14_class.C14_RS485('/dev/ttyUSB0')
-arValNum = [1, 2, 3]
-arTemp = c14.ReadValues('T', 100, 21, arValNum)
+arTemp = c14.ReadValues('T', 100, 21, [1, 2, 3])
 print(arTemp)
