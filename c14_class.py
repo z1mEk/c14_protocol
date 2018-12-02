fc123033e52b6222330ce87e57571fbed052697f
Gabriel <gabriel.zima@gmail.com>
2018-12-02 12:24:43 +0100

    Update c14_class.py

--- c14_class.py
+++ c14_class.py
@@ -14,80 +14,57 @@ class C14_RS485:
     def __init__(self, SerialPort):
         self.SerialPort = SerialPort
         self.BaudRate = 9600
-        self.bFrame = bytearray(30)
 
         logging.basicConfig(filename='/home/pi/C14_class.log', level=logging.DEBUG) # For silent set logging.CRTITICAL, please set path to log file.
         logging.debug('Started')
 
-    # Calculate checksum
-    # @param self, bytearray(30) bFrame
-    # @return byte
-    def CalcChecksum(self, bFrame):
-        i = 0
-        cSum = 0
-        for x in bFrame:
-            if i != 2:
-                cSum += x
-            i += 1
-        return cSum & 0x7f
-
     # Read frame from serial port
     # @param self, bytearray(30) bFrame
-    # @return int
-    def SerialRequest(self):
-        self.bFrame[2] = self.CalcChecksum(self.bFrame)
-        self.bFrame[29] = ord('#')
+    # @return intgb = bytearray(30)
+    def SerialRequest(self, bFrame):
         try:
             logging.debug('Serial initial...')
             ser = serial.Serial(self.SerialPort, self.BaudRate, timeout=30)
             ser.setRTS(1) # RTS=1,~RTS=0 so ~RE=0, Receive mode enabled for MAX485
             ser.setDTR(1)
             logging.debug('Write query...')
-            logging.debug('Send data: ' + str(self.bFrame))
-            ser.write(self.bFrame) # send request frame
+            logging.debug('Send data: ' + str(bFrame))
+            ser.write(bFrame) # send request frame
             logging.debug('OK')
             print('OK')
             time.sleep(3) # set empirically
             logging.debug('Read frame...')
             print('Read frame...')
-            dd = ser.read(size=30)
 #            dd = ser.read_until(expected='#')
-            print('after read')
-            print(len(dd))
-            self.bFrame = bytearray(ser.read(size=len(self.bFrame))) # receive request frame
-            logging.debug('Receive data: ' + str(self.bFrame))
+            bFrame = ser.read(size=len(bFrame)) # receive request frame
+            logging.debug('Receive data: ' + str(bFrame))
             logging.debug('OK')
             print('OK')
             ser.close()
         except serial.SerialException:
             logging.debug('Serial error')
-#            continue
-
-        if self.CalcChecksum(self.bFrame) == self.bFrame[2]:
-            logging.debug('Checksum OK')
-            return 1
-        else:
-            logging.debug('Invalid Checksum')
-            return 0
+        return bFrame
 
     # Read values to array
     # @param self, char ['T'=temperature/'R'=other parameters] ValueType, byte RecipientAddress, byte SenderAddress, list [max list(6)] ValueNumbers
     # @return list
     def ReadValues(self, ValueType, RecipientAddress, SenderAddress, ValueNumbers):
-        self.bFrame = bytearray(30)
-        self.bFrame[0] = 128 + RecipientAddress
-        self.bFrame[1] = ord(ValueType)
-        self.bFrame[3] = SenderAddress
+        bFrame = bytearray(30)
+        bFrame[0] = 128 + RecipientAddress
+        bFrame[1] = ord(ValueType)
+        bFrame[3] = SenderAddress
         i = 5
         for vnr in ValueNumbers:
-            self.bFrame[i] = vnr / 128
-            self.bFrame[i + 1] = vnr % 128
+            bFrame[i] = vnr / 128
+            bFrame[i + 1] = vnr % 128
             i += 4
-        self.SerialRequest()
+        bFrame[29] = ord('#')
+        bFrame[2] = (sum(bFrame) - bFrame[2]) & 0x7f
+        rbFrame = self.SerialRequest(bFrame)
         vnr = 7
         arVal = []
         for i in range(0, len(ValueNumbers)):
-            arVal.append(self.bFrame[vnr] << 7 | self.bFrame[vnr + 1])
+            arVal.append(rbFrame[vnr] << 7 | rbFrame[vnr + 1])
             vnr += 4
         return arVal
 
